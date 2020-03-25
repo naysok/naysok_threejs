28d7a8b8698c10afabc2bb39bd796ed069bd4dd5
naysok <ysok.ysok@gmail.com>
2020-03-25 23:59:23 +0900

    [add] stats

---
+++ _module_/new_init_stats.js
@@ -0,0 +1,13 @@
+// stats js
+function initStats() {
+    var stats = new Stats();
+
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = "0px";
+    stats.domElement.style.top = "0px";
+
+    document.getElementById("Stats-output").appendChild(stats.domElement);
+    return stats;
+
+};
